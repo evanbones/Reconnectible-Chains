16314b88eff34e8176865e38d89b5ccb80e38e19
evanbones <74276616+evanbones@users.noreply.github.com>
2026-02-01 10:14:11 -0800

    Fix server crash

--- fabric/src/main/java/com/evandev/connectiblechains/platform/FabricNetworkHelper.java
+++ fabric/src/main/java/com/evandev/connectiblechains/platform/FabricNetworkHelper.java
@@ -3,10 +3,11 @@ package com.evandev.connectiblechains.platform;
 import com.evandev.connectiblechains.networking.packet.ChainAttachS2CPacket;
 import com.evandev.connectiblechains.networking.packet.ConfigSyncPayload;
 import com.evandev.connectiblechains.platform.services.INetworkHelper;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
@@ -16,21 +17,14 @@ import java.util.function.Function;
 
 public class FabricNetworkHelper implements INetworkHelper {
 
+    public FabricNetworkHelper() {
+    }
+
     @Override
     public <T> void registerClientReceiver(Class<T> type, ResourceLocation id, Function<FriendlyByteBuf, T> decoder) {
-        ClientPlayNetworking.registerGlobalReceiver(id, (client, handler, buf, responseSender) -> {
-            T packet = decoder.apply(buf);
-
-            client.execute(() -> {
-                if (packet instanceof ChainAttachS2CPacket p) {
-                    if (client.player != null) {
-                        ChainAttachS2CPacket.handle(p, client.player);
-                    }
-                } else if (packet instanceof ConfigSyncPayload p) {
-                    ConfigSyncPayload.handle(p);
-                }
-            });
-        });
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            ClientNetworkHandler.register(id, decoder);
+        }
     }
 
     @Override
@@ -68,6 +62,24 @@ public class FabricNetworkHelper implements INetworkHelper {
             for (ServerPlayer player : PlayerLookup.all(server)) {
                 ServerPlayNetworking.send(player, packetId, buf);
             }
+        }
+    }
+
+    private static class ClientNetworkHandler {
+        static <T> void register(ResourceLocation id, Function<FriendlyByteBuf, T> decoder) {
+            net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking.registerGlobalReceiver(id, (client, handler, buf, responseSender) -> {
+                T packet = decoder.apply(buf);
+
+                client.execute(() -> {
+                    if (packet instanceof ChainAttachS2CPacket p) {
+                        if (client.player != null) {
+                            ChainAttachS2CPacket.handle(p, client.player);
+                        }
+                    } else if (packet instanceof ConfigSyncPayload p) {
+                        ConfigSyncPayload.handle(p);
+                    }
+                });
+            });
         }
     }
 }
